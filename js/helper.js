d41ef412e6af7ddc71fb721d93e18cbeee9af152
laumujica <lauraamujica@gmail.com>
2024-06-05 20:26:52 -0300

    color + icon update

---
+++ js/helper.js
@@ -0,0 +1,32 @@
+export const nouns = [
+    "dog",
+    "cat",
+    "person",
+    "ninja",
+    "pony",
+    "Ed",
+    "programming language",
+    "football",
+    "comfy chair",
+    "fake flower",
+    "giant red panda",
+    "big piñata",
+    "expired cake"
+  ];
+  
+export const actions = [
+    "bought",
+    "played with",
+    "had a coffee with",
+    "is",
+    "jumped over",
+    "entered the room with",
+    "read a book about",
+    "talked to",
+    "jumped on",
+    "is running with",
+    "is having dinner with",
+    "ran a marathon with"
+  ];
+  
+export const articles = ["the", "a", "some"];
