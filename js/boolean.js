961d504dc4abee99ae32898de866ae94cd6934bc
laumujica <lauraamujica@gmail.com>
2024-06-05 20:40:58 -0300

    Colors + Cat icon

--- js/boolean.js
+++ js/boolean.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const divEl = document.createElement(`div`);
 
 divEl.innerHTML = `
@@ -9,15 +8,3 @@ divEl.innerHTML = `
 `;
 
 document.body.append(divEl);
-=======
-const divEl = document.createElement(`div`);
-
-divEl.innerHTML = `
-  <p style="color: white; position: fixed; bottom: 10px; left: 10px;animation: shake 0.5s ;">
-    Made with 💚 by Boolean.
-    <a style="color: white; " href="https://boolean.co.uk/apply" target="_blank">Apply today!</a>
-  </p>
-`;
-
-document.body.append(divEl);
->>>>>>> 6f3f1c6a005b4848ec4583f9353019a812c37372
